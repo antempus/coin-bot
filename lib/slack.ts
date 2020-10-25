94c28c7d70a9e0cb7cf35d777368da744055d99b
rosteele <rosteele@starbucks.com>
2020-10-24 19:57:29 -0700

    linting changes

--- lib/slack.ts
+++ lib/slack.ts
@@ -1,22 +1,22 @@
-import { UsergroupsUsersListArguments, UsersInfoArguments, WebClient } from '@slack/web-api'
+import { WebClient } from '@slack/web-api'
 import { Agent } from 'https'
 const agent = new Agent({
     keepAlive: true,
     maxSockets: 30
 })
-export class SlackWebClient extends WebClient{
+export class SlackWebClient extends WebClient {
     groupId: string
-    constructor(token: string, groupId: string){
-        super(token, {agent})
+    constructor(token: string, groupId: string) {
+        super(token, { agent })
         this.groupId = groupId
     }
 
     async validateCaller(user: string): Promise<Boolean> {
-        const users = await this.usergroups.users.list({usergroup: this.groupId}).then(({users }) => users as string[])
+        const users = await this.usergroups.users.list({ usergroup: this.groupId }).then(({ users }) => users as string[])
         return new Set(users).has(user);
     }
 
-    isTargetSelf(caller: string, target: string){
+    isTargetSelf(caller: string, target: string) {
         return caller === target
     }
 }
